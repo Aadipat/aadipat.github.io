9feb362216b9a769f8af036be611e5ae1b624bc9
Aadipat <56366235+Aadipat@users.noreply.github.com>
2026-06-05 10:16:29 +0000

    Deploying to gh-pages from @ Aadipat/aadipat.github.io@38f985462e0269dce1d3560a423f9779ae9183ce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,21 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-my-webpage-is-live",
+        },{id: "post-my-webpage-is-live",
         
           title: "My webpage is live!",
         
@@ -86,52 +72,7 @@ ninja.data = [{
           section: "News",},{id: "news-our-demo-paper-qec-lake-data-lake-for-ai-enhanced-quantum-error-correction-has-been-accepted-at-vldb-2026",
           title: 'Our demo paper QEC lake: Data Lake for AI-enhanced Quantum Error Correction has...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-rubik-39-s-cube-simulation-api",
+          section: "News",},{id: "projects-rubik-39-s-cube-simulation-api",
           title: 'Rubik&amp;#39;s cube simulation API',
           description: "",
           section: "Projects",handler: () => {
